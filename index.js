bba2af18099c4efaff2762fd24bdcfde9270865c
Antoni Angga <antoniangga14@gmail.com>
2017-06-06 20:17:08 +0700

    sudah selesai tinggal helpernya

--- index.js
+++ index.js
@@ -3,3 +3,17 @@
 import DBModel from "./models/db_model.js";
 import Cohort from "./models/cohort.js";
 import Student from "./models/student.js";
+var dbModel = new DBModel('./db/student.db');
+
+let argv = process.argv
+if(argv[2] == 'playtime'){
+  console.log('-----Welcome-------');
+  var repl = require('repl');
+  var replServer = repl.start();
+  replServer.context.dbModel = dbModel;
+  replServer.context.Student = Student;
+  replServer.context.Cohort = Cohort;
+} 
+// else {
+//   help();
+// }
